f74f631afe1e7f8dbdd718971d8bada101cc96e8
titus9527 <titus9527@gmail.com>
2019-08-25 23:35:22 +0800

    init

---
+++ server.js
@@ -0,0 +1,50 @@
+var http = require('http');
+var url = require('url');
+var path = require('path');
+
+var fs = require('fs');
+
+var server = http.createServer(handleRequest);
+server.listen(8080);
+
+console.log('Server start on port 8080!');
+
+function handleRequest(req, res) {
+    var pathname = req.url;
+    if (pathname == '/') {
+        pathname = '/index.html';
+    }
+
+    var ext = path.extname(pathname)
+
+    var typeExt = {
+        '.html': 'text/html',
+        '.css': 'text/css',
+        '.js': 'text/javascript'
+    }
+
+    var contentType = typeExt[ext] || 'text/plain';
+
+    fs.readFile(__dirname + pathname, (err, data) => {
+        if (err) {
+            res.writeHead(500);
+            return res.end('Error loading' + pathname);
+        }
+
+        res.writeHead(200, { "Content-Type": contentType });
+        res.end(data)
+    })
+}
+
+var io = require("socket.io").listen(server);
+io.sockets.on("connection", socket => {
+    console.log('we have a new client:' + socket.id);
+    socket.on('mouse', data => {
+        console.log('received: ' + data.x0 + ' ' + data.y0 + ',' + data.x1 + ' ' + data.y1)
+        socket.broadcast.emit('drawing', data)
+    })
+
+    socket.on('disconnect', () => {
+        console.log('Client has disconnect!')
+    })
+})
---
+++ sketch.js
@@ -0,0 +1,72 @@
+var socket;
+var pg;
+var current = {
+    color: '#FF963B',
+    weight: 1
+}
+var sliderDom = document.getElementById('ranger-line');
+sliderDom.addEventListener('change', e => {
+    current.weight = e.target.value
+}, false);
+
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    pg = createGraphics(windowWidth, windowHeight);
+    pg.background(255);
+    socket = io.connect('http://localhost:8080');
+    socket.on('drawing', data => {
+        console.log('Got: ' + data.x0 + ' ' + data.y0 + ',' + data.x1 + ' ' + data.y1)
+
+        pg.stroke(data.color);
+        pg.strokeWeight(data.weight);
+
+        pg.line(data.x0 * width, data.y0 * height, data.x1 * width, data.y1 * height);
+    })
+}
+
+function draw() {
+    image(pg, 0, 0);
+}
+function drawLine(x0, y0, x1, y1) {
+    pg.stroke(current.color);
+    pg.strokeWeight(current.weight);
+    pg.line(x0, y0, x1, y1);
+    console.log('sendmouse:' + x0 + ' ' + y0 + ',' + x1 + ' ' + y1);
+    socket.emit('mouse', {
+        x0: x0 / width,
+        y0: y0 / height,
+        x1: x1 / width,
+        y1: y1 / height,
+        color: current.color,
+        weight: current.weight
+    })
+}
+function mousePressed() {
+    if (mouseButton === LEFT) {
+        current.x = mouseX;
+        current.y = mouseY;
+    }
+}
+
+function mouseDragged() {
+    if (mouseButton === LEFT && event.target.tagName === 'CANVAS') {
+        drawLine(current.x, current.y, mouseX, mouseY);
+        current.x = mouseX;
+        current.y = mouseY;
+    }
+}
+
+function windowResized() {
+    if (windowWidth > width || windowHeight > height) {
+        resizeCanvas(windowWidth, windowHeight);
+        var oldPg = pg;
+        pg = createGraphics(windowWidth, windowHeight);
+        pg.background(255);
+        pg.image(oldPg, 0, 0)
+    }
+}
+
+function changeColor(color) {
+    current.color = `#${color}`
+}
